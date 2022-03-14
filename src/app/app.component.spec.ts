dba84fce7b8e706c26c2eb28f264c5df2c26db3f
Felipe Alves <felipealvesferreira05@outlook.com>
2022-03-14 12:49:54 -0300

    UploadFinal

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CalculadoraModule} from './calculadora';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -7,6 +8,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        CalculadoraModule
+      ]
     }).compileComponents();
   });
 
@@ -14,12 +18,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'calculadora'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('calculadora');
   });
 
   it('should render title', () => {
